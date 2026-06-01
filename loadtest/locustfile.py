cba4585467daa541886c225620fa6cf632f6f02e
Gustavo Contardi <lito.contardi@hotmail.com>
2026-05-31 22:13:18 -0300

    pu pu push

--- loadtest/locustfile.py
+++ loadtest/locustfile.py
@@ -1,3 +1,13 @@
-# TODO: Locust load test
-# from locust import HttpUser, task
-# Hit /health and /api/nodes endpoints
+from locust import HttpUser, task, between
+
+
+class NodeRegistryUser(HttpUser):
+    wait_time = between(1, 3)
+
+    @task(3)
+    def health_check(self):
+        self.client.get("/health")
+
+    @task(2)
+    def list_nodes(self):
+        self.client.get("/api/nodes")
